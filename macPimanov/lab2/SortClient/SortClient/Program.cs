5ba398fe709b4a0926f3c90802506fb4dcd6cae3
macPimanov <macpimanov@gmail.com>
2015-12-27 00:08:31 +0300

    Добавлены лабораторные работы 2 и 3

---
+++ macPimanov/lab2/SortClient/SortClient/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using SortLibrary;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+namespace SortClient
+{
+    class Shell
+    {
+        TcpChannel kanal;
+        SharedObject obj;
+        int[] arr;
+        int max;
+        int min;
+        Task task;
+
+        public Shell()
+        {
+            kanal = new TcpChannel();
+            ChannelServices.RegisterChannel(kanal, false);
+            obj = (SharedObject)Activator.GetObject(typeof(SortLibrary.SharedObject), "tcp://localhost:8081/DataPool");
+        }
+
+        public int sort()
+        {
+            //try
+            //{
+                task = obj.GetTask();
+                if (task == null)
+                    return 0;
+
+                arr = obj.FetchData(task);
+               
+                Console.Out.WriteLine("Полученные данные:");
+                display();
+
+                int  outer;
+
+               
+                for (outer = 0; outer < task.stop - task.start; outer++)
+                {
+                    for (int j = outer + 1; j < task.stop - task.start; j++)
+                    {
+                        if (arr[j] < arr[outer])
+                        {
+                            var temp = arr[outer];
+                            arr[outer] = arr[j];
+                            arr[j] = temp;
+                        }
+                    }
+
+                    
+                }
+
+
+                Console.Out.WriteLine("Обработанные данные:");
+                display();
+                obj.Finish(task, arr);
+            return 1;
+        }
+
+        void display()
+        {
+            for (int i = 0; i < task.stop - task.start; i++)
+            {
+                Console.Out.Write(arr[i]);
+                Console.Out.Write("  ");
+            }
+            Console.Out.WriteLine();
+        }
+    }
+
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Shell shellObj = new Shell();
+            Console.Out.WriteLine("Клиент запущен");
+
+            while (shellObj.sort() != 0)
+                Console.In.ReadLine();
+
+            Console.Out.WriteLine("Заданий нет");
+            Console.ReadLine();
+            
+
+        }
+    }
+}
---
+++ macPimanov/lab2/SortServer/SortLibrary/SharedObject.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortLibrary
+{
+    public class SharedObject : MarshalByRefObject
+    {
+    
+        static int number;
+        int minim = 10000;
+        int maxim = 0;
+
+        public List<int> maxes = new List<int>();
+        public List<int> mins = new List<int>();
+
+        const int dataCount = 100; // Количество элементов в массиве
+        const int tasksCount = 2; // Максимальное кол-во задач
+
+        Queue<Task> pendingTasks; // Очередь задач, ожидающих обработки
+        Object tasksLock;
+
+        int[] dataArray;
+        int[] MinMax;
+        Object dataLock;
+
+        public SharedObject()
+        {
+            Log.Print("Create tasks and data");
+            pendingTasks = new Queue<Task>();
+            GenerateData();
+            GenerateTasks();
+
+            tasksLock = new Object();
+            dataLock = new Object();
+        }
+
+        void GenerateTasks()
+        {
+            Task temp;
+
+            int step = dataCount / tasksCount; // Разделение массива на каждую задачу
+
+            for (int i = 0; i < tasksCount; i++)
+            {
+                temp = new Task();
+                temp.start = i * step;
+                temp.stop = temp.start + step - 1;
+                pendingTasks.Enqueue(temp);
+            }
+
+        }
+
+        void GenerateData()
+        {
+            Random r = new Random();
+            dataArray = new int[dataCount];
+
+            for (int i = 0; i < dataCount; i++)
+                dataArray[i] = r.Next(0, dataCount * tasksCount);
+        }
+
+        static int[] Sort(int[] buff)
+        {
+            //Проверка длины массива, если она равна 1, то возвращаем массив, так как он не нуждается в сортировке
+            if (buff.Length > 1)
+            {
+                //Массивы для хранения половин входящего буфера
+                int[] left = new int[buff.Length / 2];
+                //Для проверки ошибки некорректного разбиения массива, в случае, если длина - непарное число
+                int[] right = new int[buff.Length - left.Length];
+
+                //Заполнение подмассивов данными из входящего массива
+                for (int i = 0; i < left.Length; i++)
+                {
+                    left[i] = buff[i];
+                }
+                for (int i = 0; i < right.Length; i++)
+                {
+                    right[i] = buff[left.Length + i];
+                }
+                //Если длина субмассивов больше единицы, то мы повторно рекурсивным методом вызываем функцию разбиения массива
+                if (left.Length > 1)
+                    left = Sort(left);
+                if (right.Length > 1)
+                    right = Sort(right);
+                //Сортировка слиянием половин
+                buff = MergeSort(left, right);
+            }
+            //Возврат отсортированного массива
+            return buff;
+        }
+
+        static int[] MergeSort(int[] left, int[] right)
+        {
+
+            //Буфер для отсортированного массива
+            int[] buff = new int[left.Length + right.Length];
+            //Счетчики длины трех массивов
+            int i = 0;  //Соединенный массив
+            int l = 0;  //Левый массив
+            int r = 0;  //Правый массив
+            //Сортировка сравнением элементов
+            for (; i < buff.Length; i++)
+            {
+                //Если правая часть уже использована, дальнейшее движение происходит только в левой
+                //Проверка на выход правого массива за пределы
+                if (r >= right.Length)
+                {
+                    buff[i] = left[l];
+                    l++;
+                }
+                //Проверка на выход за пределы левого массива и сравнение текущих значений обоих массивов
+                else if (l < left.Length && left[l] < right[r])
+                {
+                    buff[i] = left[l];
+                    l++;
+                }
+                else
+                {
+                    buff[i] = right[r];
+                    r++;
+                    //Подсчет количества инверсий
+                    if (l < left.Length)
+                        number += left.Length - l;
+                }
+            }
+            //Возврат отсортированного массива
+            return buff;
+        }
+
+        public int[] FetchData(Task task)
+        {
+            Log.Print("Клиент получил данные");
+            int[] temp = new int[task.stop-task.start];
+            int j = 0;
+            for (int i = task.start; i < task.stop; i++)
+            {
+                temp[j] = dataArray[i];
+                j++;
+            }
+
+            return temp;
+        }
+
+        public Task GetTask()
+        {
+            Log.Print("Клиент запросил задание");
+            lock (tasksLock)
+            {
+                if (pendingTasks.Count == 0)
+                {
+                    Log.Print("Заданий нет");
+                    return null;
+                }
+                else
+                    return pendingTasks.Dequeue();
+            }
+        }
+
+        public void Finish(Task task, int[] data)
+        {
+          
+            Log.Print("Клиент завершил задание");
+            lock (dataLock)
+            {
+                int j = 0;
+                for (int i = task.start; i < task.stop; i++)
+                {
+                    
+                    dataArray[i] = data[j];
+                    j++;
+                    Console.Out.Write(dataArray[i]+"  ");
+                    
+                }
+                Console.Out.WriteLine();
+
+            }
+            if (pendingTasks.Count == 0)
+            {
+                Log.Print("Последнее задание завершено");
+                Console.Out.WriteLine("\n\n");
+                Console.Out.WriteLine("Сортировка массива(Клиент)");
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.Out.Write(dataArray[i] + " ");
+                }
+                Console.Out.WriteLine("\n\n");
+                dataArray = Sort(dataArray);
+
+                Console.Out.WriteLine("Сортировка массива(Сервер)");
+                for (int i = 0; i < 100; i++)
+                {
+                   Console.Out.Write( dataArray[i]+" ");
+                }
+                Console.Out.WriteLine();
+            }
+        }
+    }
+
+
+
+    [Serializable]
+    public class Task
+    {
+        public int start = 0;
+        public int stop = 0;
+    }
+
+    public class Log
+    {
+        public static void Print(String msg)
+        {
+            System.Console.WriteLine("[" + DateTime.Now.Hour.ToString() + ":" +
+                DateTime.Now.Minute.ToString() + ":" + DateTime.Now.Second.ToString() 
+            + "] " + msg);
+        }
+    }
+}
