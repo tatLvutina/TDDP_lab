4444c80ef3f716cbb44490147625458d793b3361
FrostTea <dantevsvergil@yandex.ru>
2015-12-29 06:01:55 +0300

    labs 2 and 3

---
+++ Belozertchev/lab2/SortServer/SortServer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using SortLibrary;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+namespace SortServer
+{
+    class Server
+    {
+        TcpChannel channel;
+        public void Start()
+        {
+            channel = new TcpChannel(8081);
+            ChannelServices.RegisterChannel(channel, false);
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(SharedObject), "DataPool", WellKnownObjectMode.Singleton);
+            Log.Print("Server has started");
+        }
+
+        public void Stop()
+        {
+            ChannelServices.UnregisterChannel(channel);
+            Log.Print("Server has stopped");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server srv = new Server();
+            srv.Start();
+            Console.In.ReadLine();
+            srv.Stop();
+        }
+    }
+}
