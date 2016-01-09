9fe1ff0be85ef0881faf050827dcaf89a8d81113
Anfa11 <ilyasyava@gmail.com>
2016-01-09 03:21:48 +0300

    Добавлена 2 и 3 лабораторные работы

---
+++ Ануфриев/lab2/RemoteServer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using RemoteServices;
+
+namespace RemoteServer  
+{
+    class Program
+    {
+        private TcpChannel channel;
+
+        static void Main(string[] args)
+        {
+            new Program().start();
+        }
+
+        public void start()
+        {
+            channel = new TcpChannel(8080);  //иниц. порта
+            ChannelServices.RegisterChannel(channel, false);
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(RemoteTask), "ShellSort", WellKnownObjectMode.Singleton); //регистрация типа решаемых задач, кот. определен в RemoteTask
+            Console.WriteLine("Сервер успешно запущен.\nНажмите любую клавишу для завершения...");
+            Console.ReadKey(true);
+            Console.WriteLine("Завершение работы сервера...");
+        }
+
+        ~Program()
+        {
+            if (channel != null)
+            {
+                ChannelServices.UnregisterChannel(channel);
+            }
+        }
+    }
+}
