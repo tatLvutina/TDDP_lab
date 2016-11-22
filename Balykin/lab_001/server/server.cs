3d5e8dd11275f6ea3c4275faee1c0481ff1033c3
dima4871 <dima4871@yandex.ru>
2016-11-22 10:01:37 +0300

    Project creation

---
+++ Balykin/lab_001/server/server.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+    class server
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
