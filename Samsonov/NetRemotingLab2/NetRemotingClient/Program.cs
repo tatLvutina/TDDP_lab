cbab9d8ecd7c4d9a8624b4fbb8c28207a8fe7881
blsam <blsam@users.noreply.github.com>
2016-11-22 21:54:40 +0300

    Added lab2

---
+++ Samsonov/NetRemotingLab2/NetRemotingClient/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Windows.Forms;
+
+using NetRemotingLibrary;
+
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Services;
+using System.Runtime.Remoting;
+
+namespace NetRemotingClient
+{
+    class Program
+    {
+        const int SYS_PORT = 9232;
+        const string SYS_LIB_NAME = "Lib";
+
+
+        private static RemotingLibrary remotingClass = null;
+
+
+        static void Main(string[] args)
+        {
+
+            TcpChannel clientChannel = new TcpChannel();
+            ChannelServices.RegisterChannel(clientChannel, true);
+
+            string connectingString = string.Format("tcp://localhost:{0}/{1}", SYS_PORT,  SYS_LIB_NAME);
+            remotingClass = (RemotingLibrary)Activator.GetObject(typeof(RemotingLibrary), connectingString);
+
+            if (remotingClass == null)
+            {
+                Console.WriteLine("Client not connected! Abort!");
+                return;
+            }
+
+            bool isAdmin = false;
+            int clientID = remotingClass.RegisterClient(out isAdmin);
+
+            // admin
+            if (isAdmin)
+            {
+                Console.WriteLine("Current client connected as admin.");
+
+                const int rows = 200;
+                const int cols = 200;
+
+                int[,] task = new int[rows, cols];
+                Random r = new Random();
+                for (int i = 0; i < rows; ++i)
+                    for (int j = 0; j < cols; ++j)
+                        task[i, j] = r.Next(10);
+
+                Console.WriteLine("Uploading task...");
+                remotingClass.UploadTaskToServer(clientID, task);
+
+                Console.WriteLine("Task uploaded.");
+
+                Console.WriteLine("Press any key to disconnect admin client...");
+                Console.ReadKey();
+
+                Console.WriteLine("Disconnecting...");
+                remotingClass.UnregisterClient(clientID);
+
+                Console.WriteLine("Ended.");
+                return;
+            }
+
+            Console.WriteLine("Current client connected as processor client.");
+
+            while (!remotingClass.isWorkFinished())
+            {
+                Console.WriteLine();
+
+                int[] array = remotingClass.GetClientData(clientID);
+
+                Console.WriteLine("Client received data: ");
+                for (int i = 0; i < array.GetLength(0); ++i)
+                    Console.Write("{0} ", array[i]);
+                Console.WriteLine();
+
+                Console.WriteLine("Sorting...");
+
+                for (int i = 0; i < array.GetLength(0); ++i)
+                {
+                    for (int j = i; j < array.GetLength(0); ++j)
+                    {
+                        if (array[i] > array[j])
+                        {
+                            int a = array[i];
+
+                            array[i] = array[j];
+                            array[j] = a;
+                        }
+                    }
+                }
+
+                Console.WriteLine("Client sorted data: ");
+                for (int i = 0; i < array.GetLength(0); ++i)
+                    Console.Write("{0} ", array[i]);
+                Console.WriteLine();
+
+                remotingClass.ReturnClientData(clientID, array);
+            }
+
+            remotingClass.UnregisterClient(clientID);
+
+            Console.WriteLine("Client finished.");
+        }
+    }
+}
---
+++ Samsonov/NetRemotingLab2/NetRemotingLibrary/RemotingLibrary.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetRemotingLibrary
+{
+    public class Client
+    {
+        public enum ClientStatus { BUSY, FREE }
+
+        private int id = 0;
+        private ClientStatus status = ClientStatus.FREE;
+        private bool hadAdminRights = false;
+        private DateTime lastInteraction = DateTime.Now;
+        private int meta = 0;
+
+        public Client(int clientID)
+        {
+            id = clientID;
+            status = ClientStatus.FREE;
+            hadAdminRights = false;
+        }
+
+        public void SetAdmin(bool admin)
+        {
+            hadAdminRights = admin;
+        }
+
+        public bool GetAdmin()
+        {
+            return hadAdminRights;
+        }
+
+        public int GetID()
+        {
+            return id;
+        }
+
+        public void SetStatus(ClientStatus clientStatus)
+        {
+            status = clientStatus;
+            lastInteraction = DateTime.Now;
+        }
+
+        public DateTime GetTimeSinceLastInteraction()
+        {
+            return lastInteraction;
+        }
+
+        public ClientStatus GetStatus()
+        {
+            return status;
+        }
+
+        public int GetMeta()
+        {
+            return meta;
+        }
+
+        public void SetMeta(int clientMeta)
+        {
+            meta = clientMeta;
+        }
+    }
+
+
+    public class RemotingLibrary : MarshalByRefObject
+    {
+        public static int[] Sort(int[] array)
+        {
+            return array;
+        }
+
+
+        private static int globalID = 0;
+        private static List<Client> clients = new List<Client>();
+
+        private int[,] serverTask;
+        private bool[] rowProcessed;
+        private int[] clientData;
+
+
+        private System.Timers.Timer timer = new System.Timers.Timer();
+
+        public RemotingLibrary()
+        {
+            timer.Elapsed += timer_Elapsed;
+            timer.Interval = 1000;
+            timer.Start();
+        }
+
+        void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            lock ("timer")
+            {
+                for (int i = clients.Count - 1; i >= 0; --i)
+                {
+                    if ((clients[i].GetStatus() == Client.ClientStatus.BUSY) && (DateTime.Now - clients[i].GetTimeSinceLastInteraction()).TotalMilliseconds > 3000)
+                    {
+                        Console.WriteLine("Client {0} disconnects!", clients[i].GetID());
+
+                        int rowID = clients[i].GetMeta();
+                        rowProcessed[rowID] = false;
+
+                        clients.RemoveAt(i);
+                    }
+                }
+            }
+        }
+
+        private Client GetClientByID(int clientID)
+        {
+            Client client = null;
+            for (int i = 0; i < clients.Count; ++i)
+                if (clients[i].GetID() == clientID)
+                    client = clients[i];
+
+            return client;
+        }
+
+        public int RegisterClient(out bool isAdmin)
+        {
+            lock ("client_connect_request")
+            {
+                globalID++;
+
+                Client client = new Client(globalID);
+
+                bool hasAdmin = false;
+                for (int i = 0; i < clients.Count; ++i)
+                    hasAdmin |= clients[i].GetAdmin();
+
+                if (!hasAdmin)
+                {
+                    client.SetAdmin(true);
+                    isAdmin = true;
+                }
+                else
+                    isAdmin = false;
+            
+
+                clients.Add(client);
+
+                return globalID;
+            }
+        }
+
+        public void UnregisterClient(int clientID)
+        {
+            lock ("unregister_client")
+            {
+                for (int i = 0; i < clients.Count; ++i)
+                    if (clients[i].GetID() == clientID)
+                        clients.RemoveAt(i);
+            }
+        }
+
+        public void UploadTaskToServer(int clientID, int[,] task)
+        {
+            lock ("admin_upload_task")
+            {
+                Client client = GetClientByID(clientID);
+
+                if ((client == null) || (!client.GetAdmin()))
+                {
+                    Console.WriteLine("[ERROR] Only admin can give task's to server.");
+                    return;
+                }
+
+                if ((rowProcessed != null) && (!isWorkFinished()))
+                {
+                    Console.WriteLine("Server not fully completed previous work. Aborting.");
+                    return;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Admin [client ID: {0}] upload task to server...", clientID);
+
+                serverTask = new int[task.GetLength(0), task.GetLength(1)];
+                rowProcessed = new bool[task.GetLength(0)];
+                clientData = new int[task.GetLength(1)];
+
+                Console.WriteLine("Copying task...");
+
+                for (int i = 0; i < task.GetLength(0); ++i)
+                {
+                    for (int j = 0; j < task.GetLength(1); ++j)
+                        serverTask[i, j] = task[i, j];
+
+                    rowProcessed[i] = false;
+                }
+
+                Console.WriteLine("Task copied");
+            }
+        }
+
+
+        public int[] GetClientData(int clientID)
+        {
+            lock ("data_request")
+            {
+                Client client = GetClientByID(clientID);
+                int i = 0;
+                for (; i < rowProcessed.Length; ++i)
+                    if (rowProcessed[i] == false)
+                        break;
+
+                for (int j = 0; j < serverTask.GetLength(1); ++j)
+                    clientData[j] = serverTask[i, j];
+
+                rowProcessed[i] = true;
+                client.SetStatus(Client.ClientStatus.BUSY);
+                client.SetMeta(i);
+
+                return clientData;
+            }
+
+        }
+
+        public void ReturnClientData(int clientID, int[] data)
+        {
+            lock ("data_return")
+            {
+                Client client = GetClientByID(clientID);
+                int i = client.GetMeta();
+
+                for (int j = 0; j < serverTask.GetLength(1); ++j)
+                    serverTask[i, j] = data[j];
+
+                client.SetStatus(Client.ClientStatus.FREE);
+
+                bool flag = true;
+                for (i = 0; i < clients.Count; ++i)
+                    flag &= (clients[i].GetStatus() == Client.ClientStatus.FREE);
+
+                // Server ended
+                if (flag && isWorkFinished())
+                {
+                    Console.WriteLine("Server ended task");
+                    Console.WriteLine("Result is: ");
+                    for (i = 0; i < serverTask.GetLength(0); ++i)
+                    {
+                        for (int j = 0; j < serverTask.GetLength(1); ++j)
+                            Console.Write("{0} ", serverTask[i, j]);
+
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+
+
+        public bool isWorkFinished()
+        {
+            lock ("check_work_finished")
+            {
+                bool flag = true;
+                for (int i = 0; i < rowProcessed.Length; ++i)
+                    flag &= rowProcessed[i];
+
+                return flag;
+            }
+        }
+    }
+}
