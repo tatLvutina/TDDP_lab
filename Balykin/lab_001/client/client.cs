0402420c04cca8848ec55641f69aa17b793a0db6
dima4871 <dima4871@yandex.ru>
2016-11-22 12:11:41 +0300

    Refactoring code

--- Balykin/lab_001/client/client.cs
+++ Balykin/lab_001/client/client.cs
@@ -1,13 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-
-using System.Runtime.Remoting.Channels.Tcp;
-using System.Runtime.Remoting.Channels;
-using System.Runtime.Remoting.Services;
-using System.Runtime.Remoting;
 
 namespace client
 {
--- Balykin/lab_001/server/server.cs
+++ Balykin/lab_001/server/server.cs
@@ -1,14 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using System.Runtime.Remoting;
 using System.Runtime.Remoting.Channels;
 using System.Runtime.Remoting.Channels.Tcp;
-using System.Runtime.Remoting.Services;
-using System.Security.Permissions;
 
 namespace server
 {
